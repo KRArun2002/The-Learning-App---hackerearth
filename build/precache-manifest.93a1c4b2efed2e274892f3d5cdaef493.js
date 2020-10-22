d8a884f2dd3a9f3861ecb237f2ec76cd466a1be5
ArunKR <55146554+KRArun2002@users.noreply.github.com>
2020-10-23 02:07:12 +0530

    Add files via upload

---
+++ build/precache-manifest.93a1c4b2efed2e274892f3d5cdaef493.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cee8007eb5d05cf671ff63abca642acd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94cc67b41f5a7693f7c9",
+    "url": "/static/css/2.de23b42c.chunk.css"
+  },
+  {
+    "revision": "a946add8fb7399bc11b7",
+    "url": "/static/css/main.3802f94d.chunk.css"
+  },
+  {
+    "revision": "94cc67b41f5a7693f7c9",
+    "url": "/static/js/2.cf5837ef.chunk.js"
+  },
+  {
+    "revision": "4b1967fa1d9c3a9541d18dcbb507ed24",
+    "url": "/static/js/2.cf5837ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a946add8fb7399bc11b7",
+    "url": "/static/js/main.4f5dc2d4.chunk.js"
+  },
+  {
+    "revision": "9ca6fc43a47605460de8",
+    "url": "/static/js/runtime-main.d09d0140.js"
+  },
+  {
+    "revision": "b195d212a64e1939bf180370cad42f63",
+    "url": "/static/media/J3.b195d212.jpg"
+  },
+  {
+    "revision": "751985f17710e0e08de09d341080ced8",
+    "url": "/static/media/bg1.751985f1.jpeg"
+  },
+  {
+    "revision": "95c8a7b6a52c3c04ec0cf13367fc74d7",
+    "url": "/static/media/bg2.95c8a7b6.jpg"
+  },
+  {
+    "revision": "ee5a4b3a2c9395c5c545e77e15b2b88c",
+    "url": "/static/media/bg3.ee5a4b3a.jpg"
+  },
+  {
+    "revision": "fceef6b4761d6290e556d011ab88b24c",
+    "url": "/static/media/bg4.fceef6b4.jpg"
+  },
+  {
+    "revision": "cfaa8b5141481abd54b7c8e8eb4e3b4f",
+    "url": "/static/media/bg5.cfaa8b51.jpg"
+  },
+  {
+    "revision": "2b7bc896fd9e04fb45e1816f79956632",
+    "url": "/static/media/bg6.2b7bc896.jpg"
+  },
+  {
+    "revision": "94992609bfceb7e0b5007801dffd0962",
+    "url": "/static/media/biology.94992609.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "4b036e4b71464c9eaeb803bb28187395",
+    "url": "/static/media/motion1.4b036e4b.png"
+  },
+  {
+    "revision": "be212e54f2b2cbbb87d0f31a6864c5a8",
+    "url": "/static/media/motion2.be212e54.png"
+  },
+  {
+    "revision": "8b9e2901e2a9c0e5498bf5da0e0492a8",
+    "url": "/static/media/motion3.8b9e2901.png"
+  },
+  {
+    "revision": "d834acceb35ec2269dd0bacf98c23ca4",
+    "url": "/static/media/motion4.d834acce.png"
+  },
+  {
+    "revision": "d602844340d4eb031a9e83acf554214a",
+    "url": "/static/media/motion5.d6028443.png"
+  },
+  {
+    "revision": "e4ca8b385db0759c36ad174edb36de94",
+    "url": "/static/media/motion6.e4ca8b38.png"
+  },
+  {
+    "revision": "04fbbb0219643fea5fbbadfacc222b7e",
+    "url": "/static/media/motion7.04fbbb02.png"
+  },
+  {
+    "revision": "7bb4e04a865d26671d6a24d675b5ac72",
+    "url": "/static/media/motion8.7bb4e04a.png"
+  },
+  {
+    "revision": "da080e0a8d860306ef1d198ffc53f266",
+    "url": "/static/media/motion9.da080e0a.png"
+  },
+  {
+    "revision": "b5a7fd58773704f9a16f1c8a12eda322",
+    "url": "/static/media/p2.b5a7fd58.png"
+  },
+  {
+    "revision": "cbc6da9f5e4bfe715cb3b7543fbdd9b1",
+    "url": "/static/media/physics2.cbc6da9f.jpg"
+  },
+  {
+    "revision": "f46c4f0c2842ed57ac3670e35faaa411",
+    "url": "/static/media/slide_image1.f46c4f0c.gif"
+  },
+  {
+    "revision": "a9fa3a97de37f6cc7d59d46d11421332",
+    "url": "/static/media/slide_image2.a9fa3a97.png"
+  },
+  {
+    "revision": "32f4960a6b61ce6dc3814524ef66a13c",
+    "url": "/static/media/slide_image3.32f4960a.jpg"
+  },
+  {
+    "revision": "9b1a9a6726a8866bbcd3536c3ebfbeb2",
+    "url": "/static/media/top_pic.9b1a9a67.gif"
+  }
+]);
